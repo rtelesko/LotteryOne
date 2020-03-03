a41c76516e0345a740b4e8db803e9f6679237559
rainer.telesko <rainer.telesko@fhnw.ch>
2020-03-03 15:26:24 +0100

    Second commit

--- app/src/main/java/com/example/lotteryone/MainActivity.java
+++ app/src/main/java/com/example/lotteryone/MainActivity.java
@@ -6,6 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Essentials for programming
+    Android APK creation process: https://www.quora.com/What-exactly-happens-during-an-Android-APK-creation-build-process
+    Build options in Android Studio: https://developer.android.com/studio/run
+    Git and Github integration in Android Studio: https://www.youtube.com/watch?v=bcduIvadmPo
+    Debugging with Android Studio: https://www.youtube.com/watch?v=nBaL78HC0ls
+    Essentials:
+    Step Over command: This command lets the application execute the next action. If the action involves a call to an operation, it does not step into its implementation (it steps over it instead).
+    Step Into command: This command lets the application execute the next action. If the action involves a call to an operation, it steps into its implementation and breaks the execution on the first action of that implementation.
+    Step Out command: This command lets the application execute until the currently executed operation implementation is returned.
+    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
